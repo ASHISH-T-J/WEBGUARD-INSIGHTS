f13f9e57ba3cdc8eb5a5469fd3e217b0e61f164a
Ashish Thomas Joseph <75501870+ASHISH-T-J@users.noreply.github.com>
2024-02-09 03:08:14 +0530

    Update req.sh

--- req.sh
+++ req.sh
@@ -1,4 +1,5 @@
 sudo apt update -y
+sudo apt install git -y
 sudo apt install whatweb -y
 sudo apt install nslookup -y
 sudo apt install dig -y
@@ -6,4 +7,5 @@ sudo apt install nmap -y
 sudo apt install nikto -y
 sudo apt install whois -y
 sudo apt install wafw00f -y
+sudo apt install theHarvester -y
 sudo apt upgrade -y
