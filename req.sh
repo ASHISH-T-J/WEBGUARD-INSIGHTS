aa2077338276616086014e26ffd3184d37aabcdf
Ashish Thomas Joseph <75501870+ASHISH-T-J@users.noreply.github.com>
2024-02-08 12:07:26 +0530

    Add files via upload
    under development

---
+++ req.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt install whatweb -y
+sudo apt install nslookup -y
+sudo apt install dig -y
+sudo apt install nmap -y
+sudo apt install nikto -y
+sudo apt install whois -y
+sudo apt install wafw00f -y
+sudo apt upgrade -y
